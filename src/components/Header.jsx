d876b38840628b28b90dc6fd9dfd9309bebe1dc7
Josephine Lindley <Josephine.Lindley@MacBook-Pro-2.local>
2023-01-13 10:50:52 +0000

    adds logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import '../css/header.css'
+import logo from '../logo.png'
 
 const Header = () => {
     return (<section className="header-main">
         <section className='header-left'>
-            <div className='logo-and-brandName'>
-            <div className='logo'></div>
+            {/* <div className='logo-and-brandName'>
+            <div className='logo'>
+                <img className='logo-img' src={logo} alt="logo" />
+            </div>
             <div className='brandName'>
                 <p className='brand-title'>NEAL'S YARD REMEDIES</p>
                 <p className='brand-desc'>Natural & organic health & beauty</p>
             </div>
-            </div>
+            </div> */}
+            <img src={logo} alt="logo" />
         </section>
     </section> );
 }
