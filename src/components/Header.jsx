2989ae452573f2d475696b2a79b51cd1badeedac
Josephine Lindley <Josephine.Lindley@MacBook-Pro-2.local>
2023-01-13 09:57:10 +0000

    first commit, adds header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import '../css/header.css'
+
+const Header = () => {
+    return (<section className="header-main">
+        <section className='header-left'>
+            <div className='logo-and-brandName'>
+            <div className='logo'></div>
+            <div className='brandName'>
+                <p className='brand-title'>NEAL'S YARD REMEDIES</p>
+                <p className='brand-desc'>Natural & organic health & beauty</p>
+            </div>
+            </div>
+        </section>
+    </section> );
+}
+ 
+export default Header;
